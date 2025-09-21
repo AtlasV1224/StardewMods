1c1f5c306cac669023f9953b1ebdf40df3fce8fa
AtlasV <40894199+AtlasV1224@users.noreply.github.com>
2025-09-21 08:29:13 +0100

    First attempt ad customisable spouse rooms

---
+++ __ManifestGrabber/Main.py
@@ -0,0 +1,6 @@
+from ManifestDataGrabber import extract_manifests
+
+results = extract_manifests(output_filename="my_manifests.json")
+print(results)
+
+results = f'### [{Name}](https://www.nexusmods.com/stardewvalley/mods/{FirstListEntry})'
